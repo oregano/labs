b0bf006d2996c3faeebfef0d0bfabe5c83077c31
Kaifee Mohammad <kaifee.mohammad@outlook.com>
2019-01-30 00:09:34 +0000

    express

--- Amity-Lab/alive.js
+++ Amity-Lab/alive.js
@@ -1,1 +1,15 @@
-console.log('I am alive !!!!!!!!!!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Hi, I am a web server...');
+})
+
+app.get('/getdetails', (req, res) => {
+    return res.send(`This is ${req.query.name} and I am ${req.query.age} old.`);
+})
+
+app.listen(4000, (err) => {
+    console.log('I\'m listening');
+});
