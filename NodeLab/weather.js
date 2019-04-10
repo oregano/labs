ab00b1c263d992264ddddf666f3ec121fc71424b
Kaifee <43731594+KaifeeM@users.noreply.github.com>
2019-04-11 00:03:07 +0100

    weather map

--- NodeLab/weather.js
+++ NodeLab/weather.js
@@ -1,12 +1,17 @@
-var agent = require('superagent');
+const agent = require('superagent');
+const util = require('util');
 
 const key = 'f7062f01c1a4f3a56374f39ab8afa7e6';
-const city = 'Hyderabad, India';
-var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+const place = 'Chelmsford, GB';
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${key}`
 
-agent.get(url, (err, res) =>{
-    if(err) throw err;
+agent.get(url, (err, res) => {
+
+    if (err) throw err;
 
     var data = JSON.parse(res.text);
-    console.log(data.main.temp - 273.15);
+
+    var temp = data.main.temp - 273.15 // converting into celsius
+    var type = data.weather[0].description;
+    console.log(`Current temperature is ${temp} celsius with ${type}.`);
 })
